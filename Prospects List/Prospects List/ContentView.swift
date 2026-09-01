54eeeccbfc1e3db10fcc87bdc74df89822a56d71
Israel Manzo <israelmanzo814@gmail.com>
2026-08-31 20:54:09 -0400

    added new project for prospects list take home

---
+++ Prospects List/Prospects List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prospects List
+//
+//  Created by Israel Manzo on 8/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prospects List/Prospects List/Prospects_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prospects_ListApp.swift
+//  Prospects List
+//
+//  Created by Israel Manzo on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Prospects_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
